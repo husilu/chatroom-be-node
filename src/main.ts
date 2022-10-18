57d5187e571d077a415c230d2f92f749588d9100
husilu <714429363@qq.com>
2022-10-19 00:07:45 +0800

    feature: 重写user resource 以及globalFilters和globalInterceptors

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+// 添加全局返回参数
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+// 异常处理
+// 捕获所有异常
+import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
+// 只捕获HTTP相关的异常错误
+import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
+  await app.listen(3000);
+}
+bootstrap();
